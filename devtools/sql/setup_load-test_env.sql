3e1455c83ec4a712760ccd884f05d5148d3d9327
Dmitriy Brusenin <star_maps@mail.ru>
2024-04-18 16:08:47 +0300

    update devtools

---
+++ devtools/sql/setup_load-test_env.sql
@@ -0,0 +1,12 @@
+INSERT INTO "reaction" ("id", "type", "code") VALUES
+('reaction', 'unicode', '😃'),
+('reaction1', 'unicode', '1'),
+('reaction2', 'unicode', '2'),
+('reaction3', 'unicode', '3') ON CONFLICT ("id") DO NOTHING;
+
+INSERT INTO "reaction_set" ("id", "reaction_ids") VALUES
+('reaction_set', array['reaction']) ON CONFLICT ("id") DO NOTHING;
+-- ('reaction_set', array['reaction1', 'reaction2', 'reaction3']) ON CONFLICT ("id") DO NOTHING;
+
+INSERT INTO "namespace" ("id", reaction_set_id, max_uniq_reactions, mutually_exclusive_reactions) VALUES
+('namespace', 'reaction_set', 10, array[array['reaction1', 'reaction2'], array['reaction2', 'reaction3']]) ON CONFLICT ("id") DO NOTHING;

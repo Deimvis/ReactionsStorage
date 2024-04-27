50c54e6b1731ca44c67a710f2b3325dd471de89f
Dmitriy Brusenin <star_maps@mail.ru>
2024-04-27 17:00:43 +0300

    resave no_join implementation

---
+++ tests/http_handlers/.impl_no_join/available_reactionsGET_test.go
@@ -0,0 +1,78 @@
+package http_handlers_test
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Deimvis/reactionsstorage/src/models"
+	"github.com/Deimvis/reactionsstorage/src/utils"
+	"github.com/Deimvis/reactionsstorage/tests/fake"
+	setup "github.com/Deimvis/reactionsstorage/tests/setup"
+)
+
+func TestAvailableReactionsGET_Simple(t *testing.T) {
+	var req models.AvailableReactionsGETRequest
+	req.Query.NamespaceId = fake.NamespaceId
+
+	var resp models.AvailableReactionsGETResponse200
+	resp.Reactions = []models.Reaction{
+		fake.Reaction, fake.Reaction2, fake.Reaction3,
+		fake.FreeReaction1, fake.FreeReaction2, fake.FreeReaction3,
+		fake.FreeReaction4, fake.FreeReaction5, fake.FreeReaction6,
+	}
+
+	test(t, &req, &resp)
+}
+
+func TestAvailableReactionsGET_Complex(t *testing.T) {
+	defer setup.SetFakeConfiguration()
+
+	for i, tc := range configurationPOST_testCases_Complex {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			conf := tc.Configuration
+			setup.SetConfiguration(conf)
+			for _, n := range conf.Namespaces {
+				reactionSet := conf.ReactionSets[slices.IndexFunc(conf.ReactionSets, func(rs models.ReactionSet) bool { return rs.Id == n.ReactionSetId })]
+				availableReactions := utils.Filter(conf.Reactions, func(r models.Reaction) bool { return utils.Contains(reactionSet.ReactionIds, r.Id) })
+
+				var req models.AvailableReactionsGETRequest
+				req.Query.NamespaceId = fake.NamespaceId
+
+				var resp models.AvailableReactionsGETResponse200
+				resp.Reactions = availableReactions
+
+				test(t, &req, &resp)
+			}
+		})
+	}
+}
+
+func TestAvailableReactionsGET_404(t *testing.T) {
+	defer setup.SetFakeConfiguration()
+
+	testCases := []struct {
+		namespaceId string
+	}{
+		{
+			"namespace that does not exist",
+		},
+		{
+			"namespace that does not exist 2",
+		},
+		{
+			"namespace that does not exist 3",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			var req models.AvailableReactionsGETRequest
+			req.Query.NamespaceId = tc.namespaceId
+			resp := request(t, &req)
+			require.Equal(t, 404, resp.Code)
+		})
+	}
+}
---
+++ tests/http_handlers/.impl_no_join/reactionsDELETE_test.go
@@ -0,0 +1,169 @@
+package http_handlers_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Deimvis/reactionsstorage/src/models"
+	"github.com/Deimvis/reactionsstorage/tests/fake"
+	setup "github.com/Deimvis/reactionsstorage/tests/setup"
+)
+
+func TestReactionsDELETE_Simple(t *testing.T) {
+	defer setup.ClearUserReactions()
+
+	var req models.ReactionsDELETERequest
+	req.Body.NamespaceId = fake.NamespaceId
+	req.Body.EntityId = fake.EntityId
+	req.Body.ReactionId = fake.ReactionId
+	req.Body.UserId = fake.UserId
+
+	var resp models.ReactionsDELETEResponse200
+	resp.Status = "ok"
+
+	test(t, &req, &resp)
+}
+
+func TestReactionsDELETE_Complex(t *testing.T) {
+	defer setup.ClearUserReactions()
+
+	for _, tc := range reactionsDELETE_testCase_Complex {
+		t.Run(tc.name, func(t *testing.T) {
+			setup.SetUserReactions(tc.initialURs)
+			for _, r := range tc.requests {
+				var req models.ReactionsDELETERequest
+				req.Body = r.userReaction
+				resp := request(t, &req)
+				require.Equal(t, r.expectedStatusCode, resp.Code)
+			}
+			require.ElementsMatch(t, tc.expectedURs, setup.GetUserReactions())
+		})
+	}
+}
+
+type reactionsDELETE_testCase struct {
+	name        string
+	initialURs  []models.UserReaction
+	requests    []reactionsDELETE_request
+	expectedURs []models.UserReaction
+}
+
+type reactionsDELETE_request struct {
+	userReaction       models.UserReaction
+	expectedStatusCode int
+}
+
+var reactionsDELETE_testCase_Complex = []reactionsDELETE_testCase{
+	{
+		name: "remove 1 reaction",
+		initialURs: []models.UserReaction{
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId,
+				ReactionId:  fake.ReactionId,
+			},
+		},
+		requests: []reactionsDELETE_request{
+			{
+				userReaction: models.UserReaction{
+					NamespaceId: fake.NamespaceId,
+					EntityId:    fake.EntityId,
+					UserId:      fake.UserId,
+					ReactionId:  fake.ReactionId,
+				},
+				expectedStatusCode: 200,
+			},
+		},
+		expectedURs: []models.UserReaction{},
+	},
+	{
+		name:       "remove 1 reaction that does not exist",
+		initialURs: nil,
+		requests: []reactionsDELETE_request{
+			{
+				userReaction: models.UserReaction{
+					NamespaceId: fake.NamespaceId,
+					EntityId:    fake.EntityId,
+					UserId:      fake.UserId,
+					ReactionId:  fake.ReactionId,
+				},
+				expectedStatusCode: 200,
+			},
+		},
+		expectedURs: []models.UserReaction{},
+	},
+	{
+		name: "remove 1 reaction out of 2 same reactions",
+		initialURs: []models.UserReaction{
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId1,
+				ReactionId:  fake.ReactionId,
+			},
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId2,
+				ReactionId:  fake.ReactionId,
+			},
+		},
+		requests: []reactionsDELETE_request{
+			{
+				userReaction: models.UserReaction{
+					NamespaceId: fake.NamespaceId,
+					EntityId:    fake.EntityId,
+					UserId:      fake.UserId1,
+					ReactionId:  fake.ReactionId,
+				},
+				expectedStatusCode: 200,
+			},
+		},
+		expectedURs: []models.UserReaction{
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId2,
+				ReactionId:  fake.ReactionId,
+			},
+		},
+	},
+	{
+		name: "remove 1 reaction out of 2 different reactions",
+		initialURs: []models.UserReaction{
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId,
+				ReactionId:  fake.FreeReactionId1,
+			},
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId,
+				ReactionId:  fake.FreeReactionId2,
+			},
+		},
+		requests: []reactionsDELETE_request{
+			{
+				userReaction: models.UserReaction{
+					NamespaceId: fake.NamespaceId,
+					EntityId:    fake.EntityId,
+					UserId:      fake.UserId,
+					ReactionId:  fake.FreeReactionId1,
+				},
+				expectedStatusCode: 200,
+			},
+		},
+		expectedURs: []models.UserReaction{
+			{
+				NamespaceId: fake.NamespaceId,
+				EntityId:    fake.EntityId,
+				UserId:      fake.UserId,
+				ReactionId:  fake.FreeReactionId2,
+			},
+		},
+	},
+}
